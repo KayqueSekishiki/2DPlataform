743328ba331558ede678b109f6e0b01bde6e2256
Kayque 2 -  Win 11 <kayquelopes@outlook.com.br>
2023-02-22 20:52:55 -0300

    Script: Add Player Jump and Friction!

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -6,20 +6,43 @@ public class Player : MonoBehaviour
 {
     public Rigidbody2D myRigidBody;
 
-    public Vector2 velocity;
+    public Vector2 friction = new(.1f, 0);
     public float speed;
+    public float jumpForce;
 
     private void Update()
     {
+        HandleMovement();
+        HandleJump();
+    }
+
+    private void HandleMovement()
+    {
         if (Input.GetKey(KeyCode.LeftArrow))
         {
-            // myRigidBody.MovePosition(myRigidBody.position - velocity * Time.deltaTime);
             myRigidBody.velocity = new Vector2(-speed, myRigidBody.velocity.y);
         }
         else if (Input.GetKey(KeyCode.RightArrow))
         {
-            // myRigidBody.MovePosition(myRigidBody.position + velocity * Time.deltaTime);
             myRigidBody.velocity = new Vector2(speed, myRigidBody.velocity.y);
+        }
+
+        if (myRigidBody.velocity.x > 0)
+        {
+            myRigidBody.velocity -= friction;
+        }
+        else if (myRigidBody.velocity.x < 0)
+        {
+            myRigidBody.velocity += friction;
+
+        }
+    }
+
+    private void HandleJump()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            myRigidBody.velocity = jumpForce * Vector2.up;
         }
     }
 }
