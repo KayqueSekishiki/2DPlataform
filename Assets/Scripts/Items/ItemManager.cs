6a1b9ee5b5cef116dfaabcba425f351205735619
Kayque 2 -  Win 11 <kayquelopes@outlook.com.br>
2023-02-24 09:58:11 -0300

    Add UIInGameManager

--- Assets/Scripts/Items/ItemManager.cs
+++ Assets/Scripts/Items/ItemManager.cs
@@ -1,23 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using TMPro;
 using Ebac.Core.Singleton;
 
 public class ItemManager : Singleton<ItemManager>
 {
-    public TextMeshProUGUI uiTextCoins;
     public int coins;
-
 
     private void Start()
     {
         Reset();
-    }
-
-    private void Update()
-    {
-        UpdateCoinsTextUI();
     }
 
 
@@ -29,37 +21,11 @@ public class ItemManager : Singleton<ItemManager>
     public void AddCoins(int amount)
     {
         coins += amount;
+        UpdateCoinsTextUI();
     }
 
     public void UpdateCoinsTextUI()
     {
-        if (coins < 0)
-        {
-            uiTextCoins.text = "X " + coins + " How did you do it?";
-        }
-        else if (coins >= 0 && coins < 10)
-        {
-            uiTextCoins.text = "x 0000" + coins;
-        }
-        else if (coins >= 10 && coins < 100)
-        {
-            uiTextCoins.text = "x 000" + coins;
-        }
-        else if (coins >= 100 && coins < 1000)
-        {
-            uiTextCoins.text = "x 00" + coins;
-        }
-        else if (coins >= 1000 && coins < 10000)
-        {
-            uiTextCoins.text = "x 0" + coins;
-        }
-        else if (coins >= 10000 && coins < 100000)
-        {
-            uiTextCoins.text = "x 0" + coins;
-        }
-        else
-        {
-            uiTextCoins.text = "I'm Rich!";
-        }
+        UIInGameManager.Instance.UpdateTextCoins(coins);
     }
 }
